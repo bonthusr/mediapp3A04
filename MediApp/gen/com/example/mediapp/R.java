6958a0da10c2f7005b93e6f24d252ae9002d4798
Fady Makram <makramf@mcmaster.ca>
2013-11-24 22:19:13 -0500

    Minor changes

--- MediApp/gen/com/example/mediapp/R.java
+++ MediApp/gen/com/example/mediapp/R.java
@@ -59,6 +59,7 @@ public final class R {
         public static final int all_patients=0x7f070000;
         public static final int main=0x7f070001;
         public static final int new_patient=0x7f070002;
+        public static final int new_patient_mr=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -66,6 +67,7 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int title_activity_all_patients=0x7f050004;
         public static final int title_activity_new_patient=0x7f050003;
+        public static final int title_activity_new_patient_mr=0x7f050005;
     }
     public static final class style {
         /** 
